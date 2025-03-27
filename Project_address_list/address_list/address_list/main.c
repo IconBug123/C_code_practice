d7c545fa189b10c5ceabc9f8d7b2203ed04f1720
IconBug123 <xuanlingzi367@gmail.com>
2025-03-26 23:48:06 -0700

    update for contact

--- Project_address_list/address_list/address_list/main.c
+++ Project_address_list/address_list/address_list/main.c
@@ -1,21 +1,51 @@
-﻿// address_list.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#define _CRT_SECURE_NO_WARNINGS
 
-#include <stdio.h>
+#include "head.h"
+
+void menu() {
+    printf("***************************************\n");
+    printf("***** 1. add           2. del          ********\n");
+    printf("***** 3. search      4. modify    ********\n");
+    printf("***** 5. show        6. sort        ********\n");
+    printf("***** 0. exit                             ********\n");
+    printf("***************************************\n");
+}
+
 
 int main()
 {
+    int input = 0;
+    Contact con;
+    InitContact(&con);  //initialize this contact  用地址，从而可以修改，还可以提高效率由于是结构体变量
+    do {
+        menu();
+        printf("Please choose:> ");
+        scanf("%d", &input);
+        switch (input) {
+        case 1:
+            AddContact(&con);
+            break;
+        case 2:
+            DelContact(&con);
+            break;
+        case 3:
+            SearchContact(&con);
+            break;
+        case 4:
+            break;
+        case 5:
+            ShowContact(&con);
+            break;
+        case 6:
+            break;
+        case 0:
+            printf("Sign out.....\n");
+            break;
+        default:
+            printf("Error Choosing.....\n");
+            break;
+        }
+    } while (input);
     printf("Hello\n");
     return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
