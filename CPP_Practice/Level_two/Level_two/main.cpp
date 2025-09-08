ceb01fde4a485d4898858a9989c1028cbce13727
IconBug123 <xuanlingzi367@gmail.com>
2025-09-07 18:11:16 -0700

    123

--- CPP_Practice/Level_two/Level_two/main.cpp
+++ CPP_Practice/Level_two/Level_two/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "UI_APP.h"
+
 
 int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	UI_APP app; // 封装了 UI 交互与业务逻辑
+	app.run(); // 进入主循环
+
 	return 0;
 }
+
